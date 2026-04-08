499d11044195d9e060e373780510bf2146d21bfd
Tyler Adler <tyler.adler@gmail.com>
2026-04-08 10:57:10 -0700

    Add DAYS parameter to run_tvguide.sh script

--- run_tvguide.sh
+++ run_tvguide.sh
@@ -2,10 +2,11 @@
 
 mkdir -p /app/output
 
-echo "Running with HOST=$HOST FILENAME=$FILENAME" >> /app/output/cron.log
+echo "Running with HOST=$HOST FILENAME=$FILENAME DAYS=2" >> /app/output/cron.log
 
 python /app/HDHomeRunEPG_To_XmlTv.py \
   --host "$HOST" \
-  --filename "$FILENAME" >> /app/output/cron.log 2>&1
+  --filename "$FILENAME" \
+  --days 2 >> /app/output/cron.log 2>&1
 
 echo "TV guide updated at $(date)" >> /app/output/cron.log
