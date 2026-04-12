b685caa0c7f2c37cf3c943d5e3f3375f45e0c0e7
Tyler Adler <tyler.adler@gmail.com>
2026-04-12 08:25:16 -0700

    Fix filename path in run_tvguide.sh script

--- run_tvguide.sh
+++ run_tvguide.sh
@@ -6,7 +6,7 @@ echo "Running with HOST=$HOST FILENAME=$FILENAME DAYS=2" >> /app/output/cron.log
 
 python3 /app/HDHomeRunEPG_To_XmlTv.py \
   --host "$HOST" \
-  --filename "$FILENAME" \
+  --filename "/app/output/$FILENAME" \
   --days 2 >> /app/output/cron.log 2>&1
 
 echo "TV guide updated at $(date)" >> /app/output/cron.log
